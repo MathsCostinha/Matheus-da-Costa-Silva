4073c99529929b19a66fbb21a44a12d5b1ee215f
Matheus da Costa Silva <2017118050244@ifpi.int>
2019-05-23 09:12:46 -0300

    Primeiro commit

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,210 @@
+package aplicacao;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pedido;
+
+public class Programa {
+
+	public static void main(String[] args) throws ParseException {
+		// TODO Auto-generated method stub
+		Scanner ler = new Scanner(System.in);
+		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		System.out.println("Para listar os pedidos cadastrados: Digite 1\n"
+				+"Para buscar um pedido pelo ID: Digite 2\n"
+				+"Para cadastrar um novo pedido: Digite 3\n"
+				+"Para atualizar um pedido: Digite 4\n"
+				+"Para remover um pedido: Digite 5\n"
+				+"Para sair do Banco de Dados: Digite 0\n");
+		int escolha = ler.nextInt();
+		while(escolha != 0) {
+			if(escolha==1) {
+				String jpql = "SELECT p FROM pedido p";
+				List<Pedido>pedidos = entityManager.createQuery(jpql,Pedido.class).getResultList();
+				System.out.println(pedidos);
+				System.out.println("Se deseja fazer mais alterações no Banco de Dados:\n"
+					+"Para listar os pedidos cadastrados: Digite 1\n"
+					+"Para buscar um pedido pelo ID: Digite 2\n"
+					+"Para cadastrar um novo pedido: Digite 3\n"
+					+"Para atualizar um pedido: Digite 4\n"
+					+"Para remover um pedido: Digite 5\n"
+					+"Para sair do Banco de Dados: Digite 0\n");
+				escolha = ler.nextInt();
+			}
+			else if(escolha==2) {
+				System.out.println("Digite o ID a ser procurado: ");
+				int id = ler.nextInt();
+				Pedido pedidoFound= entityManager.find(Pedido.class,id);
+				if(pedidoFound != null) {
+					System.out.println(pedidoFound);
+					System.out.println("Se deseja fazer mais alterações no Banco de Dados:\n"
+						+"Para listar os pedidos cadastrados: Digite 1\n"
+						+"Para buscar um pedido pelo ID: Digite 2\n"
+						+"Para cadastrar um novo pedido: Digite 3\n"
+						+"Para atualizar um pedido: Digite 4\n"
+						+"Para remover um pedido: Digite 5\n"
+						+"Para sair do Banco de Dados: Digite 0\n");
+					escolha = ler.nextInt();
+				}
+				else if(pedidoFound==null ) {
+					System.out.println("ID não encontrado!");
+					System.out.println("Se deseja fazer mais alterações no Banco de Dados: \n"
+						+"Para listar os pedidos cadastrados: Digite 1\n"
+						+"Para buscar um pedido pelo ID: Digite 2\n"
+						+"Para cadastrar um novo pedido: Digite 3\n"
+						+"Para atualizar um pedido: Digite 4\n"
+						+"Para remover um pedido: Digite 5\n"
+						+"Para sair do Banco de Dados: Digite 0\n");
+					escolha = ler.nextInt();
+				}
+			}
+			else if (escolha==3) {
+				System.out.println("Digite a data: ");
+				String dataRecebida = ler.nextLine();
+				DateFormat df = new SimpleDateFormat("dd/MM/yyyy");  
+				Date dt = df.parse(dataRecebida);
+				System.out.println(dt);
+				System.out.println("Digite o valor: ");
+				float valor = ler.nextFloat();
+				Pedido pedido = new Pedido(null, dt, valor);
+				entityManager.getTransaction().begin();
+				entityManager.persist(pedido);
+				entityManager.getTransaction().commit();
+				System.out.println("Cadastro realizado com sucesso!");
+				System.out.println("Caso deseje fazer mais alterações no Banco de Dados:\n"
+					+"Para listar os pedidos cadastrados: Digite 1\n"
+					+"Para buscar um pedido pelo ID: Digite 2\n"
+					+"Para cadastrar um novo pedido: Digite 3\n"
+					+"Para atualizar um pedido: Digite 4\n"
+					+"Para remover um pedido: Digite 5\n"
+					+"Para sair do Banco de Dados: Digite 0\n");
+				escolha = ler.nextInt();
+			}
+			else if(escolha==4) {
+				System.out.println("Digite o ID a ser atualizado: ");
+				int id = ler.nextInt();
+				Pedido pedidoFound = entityManager.find(Pedido.class,id);
+				if(pedidoFound==null) {
+					System.out.println("ID não existente!");
+					System.out.println("Se deseja fazer mais alterações no Banco de Dados: \n"
+						+"Para listar os pedidos cadastrados: Digite 1\n"
+						+"Para buscar um pedido pelo ID: Digite 2\n"
+						+"Para cadastrar um novo pedido: Digite 3\n"
+						+"Para atualizar um pedido: Digite 4\n"
+						+"Para remover um pedido: Digite 5\n"
+						+"Para sair do Banco de Dados: Digite 0\n");
+					escolha = ler.nextInt();
+				}
+			else if(pedidoFound!=null) {
+				System.out.println("Se deseja atualizar:\n data: Digite 1\n valor: Digite 2\n Os dois anteriores: Digite 3");
+				int escolhaDeAlteracao = ler.nextInt();
+				if(escolhaDeAlteracao==1) {
+					System.out.println("Alterar data para: ");
+					String dataRecebida = ler.nextLine();
+					DateFormat df = new SimpleDateFormat("dd/MM/yyyy");  
+					Date dt = df.parse(dataRecebida);
+					System.out.println(dt);
+					pedidoFound.setData(dt);
+					entityManager.getTransaction().begin();
+					entityManager.persist(pedidoFound);
+					entityManager.getTransaction().commit();
+					System.out.println("Alteração realizada com sucesso!");
+					System.out.println("Se deseja fazer mais alterações no Banco de Dados: \n"
+						+"Para listar os pedidos cadastrados: Digite 1\n"
+						+"Para buscar um pedido pelo ID: Digite 2\n"
+						+"Para cadastrar um novo pedido: Digite 3\n"
+						+"Para atualizar um pedido: Digite 4\n"
+						+"Para remover um pedido: Digite 5\n"
+						+"Para sair do Banco de Dados: Digite 0\n");
+					escolha = ler.nextInt();
+				}
+				else if(escolhaDeAlteracao==2) {
+					System.out.println("Alterar valor para: ");
+					float novovalor = ler.nextFloat();
+					pedidoFound.setValor(novovalor);
+					entityManager.getTransaction().begin();
+					entityManager.persist(pedidoFound);
+					entityManager.getTransaction().commit();
+					System.out.println("Alteração realizada com sucesso!");
+					System.out.println("Se deseja fazer mais alterações no Banco de Dados: \n"
+						+"Para listar os pedidos cadastrados: Digite 1\n"
+						+"Para buscar um pedido pelo ID: Digite 2\n"
+						+"Para cadastrar um novo pedido: Digite 3\n"
+						+"Para atualizar um pedido: Digite 4\n"
+						+"Para remover um pedido: Digite 5\n"
+						+"Para sair do Banco de Dados: Digite 0\n");
+					escolha = ler.nextInt();
+				}
+				else if(escolhaDeAlteracao==3) {
+					System.out.println("Alterar data para: ");
+					String dataRecebida = ler.nextLine();
+					DateFormat df = new SimpleDateFormat("dd/MM/yyyy");  
+					Date dt = df.parse(dataRecebida);
+					System.out.println(dt);
+					pedidoFound.setData(dt);
+					System.out.println("Alterar valor para: ");
+					float novovalor = ler.nextFloat();
+					pedidoFound.setValor(novovalor);
+					entityManager.getTransaction().begin();
+					entityManager.persist(pedidoFound);
+					entityManager.getTransaction().commit();
+					System.out.println("Alteração realizada com sucesso!");
+					System.out.println("Se deseja fazer mais alterações no Banco de Dados: \n"
+						+"Para listar os pedidos cadastrados: Digite 1\n"
+						+"Para buscar um pedido pelo ID: Digite 2\n"
+						+"Para cadastrar um novo pedido: Digite 3\n"
+						+"Para atualizar um pedido: Digite 4\n"
+						+"Para remover um pedido: Digite 5\n"
+						+"Para sair do Banco de Dados: Digite 0\n");
+					escolha = ler.nextInt();
+				}
+			}
+			}
+			else if(escolha==5) {
+				System.out.println("Digite o ID que deseja remover: ");
+				int id=ler.nextInt();
+				Pedido pedidoFound= entityManager.find(Pedido.class,id);
+				if(pedidoFound!=null) {
+					entityManager.getTransaction().begin();
+					entityManager.remove(pedidoFound);
+					entityManager.getTransaction().commit();
+					System.out.println("Removido com sucesso!");
+					System.out.println("Se deseja fazer mais alterações no Banco de Dados: \n"
+						+"Para listar os pedidos cadastrados: Digite 1\n"
+						+"Para buscar um pedido pelo ID: Digite 2\n"
+						+"Para cadastrar um novo pedido: Digite 3\n"
+						+"Para atualizar um pedido: Digite 4\n"
+						+"Para remover um pedido: Digite 5\n"
+						+"Para sair do Banco de Dados: Digite 0\n");
+					escolha = ler.nextInt();
+				}
+				else if(pedidoFound==null) {
+					System.out.println("ID não existente!");
+					System.out.println("Se deseja fazer mais alterações no Banco de Dados: \n"
+						+"Para listar os pedidos cadastrados: Digite 1\n"
+						+"Para buscar um pedido pelo ID: Digite 2\n"
+						+"Para cadastrar um novo pedido: Digite 3\n"
+						+"Para atualizar um pedido: Digite 4\n"
+						+"Para remover um pedido: Digite 5\n"
+						+"Para sair do Banco de Dados: Digite 0\n");
+					escolha = ler.nextInt();
+				}
+			}	
+		}	
+		if(escolha==0) {
+			entityManager.close();
+			entityManagerFactory.close();
+			System.out.println("Não consegui mexer com a data. Goodbye!");
+		}	
+	}
+}
---
+++ src/dominio/Pedido.java
@@ -0,0 +1,64 @@
+package dominio;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pedido implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Date data;
+	private float valor;
+
+	public Pedido() {
+
+	}
+
+	public Pedido(Integer id, Date data, float valor) {
+		super();
+		this.id = id;
+		this.data = data;
+		this.valor = valor;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa [ID = " + id + ", data = " + data + ", valor = " + valor + "]";
+	}
+
+}
